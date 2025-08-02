025b875fdb71cb265929a913d9959535cc6e2b8d
Rucknium <Rucknium@protonmail.com>
2025-08-02 17:12:03 +0000

    Added text explaining alt chains graph

--- R/app_ui.R
+++ R/app_ui.R
@@ -11,7 +11,17 @@ app_ui <- function(request) {
     # Your application UI logic
     fluidPage(
       titlePanel("Monero Consensus Status"),
-      shiny::h4(shiny::HTML('The open source code for this web app is available <a href="https://github.com/Rucknium/xmrconsensus">here</a>.')),
+      shiny::h4(shiny::strong(shiny::HTML('The open source code for this web app is available <a href="https://github.com/Rucknium/xmrconsensus">here</a>.'))),
+      shiny::br(),
+      shiny::h4(shiny::HTML('This web app displays a visualization of recent <a href="https://monero.stackexchange.com/questions/3311/what-are-orphaned-blocks">orphaned blocks</a> and alternative chains of the Monero blockchain.')),
+      shiny::br(),
+      shiny::h4(shiny::HTML('Occasional orphaned blocks are normal. They occur naturally when two miners mine valid blocks almost simultaneously. A high rate of orphaned blocks can indicate a problem in network-wide connection latency or even malicious behavior by one or more entities with a large hashpower share.')),
+      shiny::br(),
+      shiny::h4(shiny::HTML('If a malicious entity with a high share of network hashpower attempted a selfish mining strategy to raise its share of block rewards, the rate of orphaned blocks could increase. The malicious entity would cause the blocks of other pools to become orphaned. This evidence would appear in the visualization below.')),
+      shiny::br(),
+      shiny::h4(shiny::HTML('A malicious entity with 50 percent or more of total network hashpower could attempt deep chain re-organizations by mining an alternative chain. Alternative chain are like orphaned blocks, but are more than one block in length. Alternative chains should appear in the visualization below.')),
+      shiny::br(),
+      shiny::h4(shiny::HTML('This web app is new and untested.')),
       shiny::br(),
       # golem::golem_welcome_page() # Remove this line to start building your UI
       plotOutput("plot1", inline = TRUE),
